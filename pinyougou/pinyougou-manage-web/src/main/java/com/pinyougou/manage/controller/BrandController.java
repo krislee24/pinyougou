71c9952cf7a321c5dbe23485bcd5fad221c9f5af
KrisLee <907222404@qq.com>
2018-10-18 20:57:51 +0800

    项目1第二天

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -3,9 +3,9 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.pinyougou.vo.PageResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +15,21 @@ public class BrandController {
     @Reference
     private BrandService brandService;
 
+    @GetMapping("/testPage")
+    public List<TbBrand> testPage(Integer page, Integer rows) {
+//        return brandService.testPage(page, rows);
+        return (List<TbBrand>) brandService.findPage(page, rows).getRows();
+    }
+
     @GetMapping("/findAll")
-    public List<TbBrand> findAll(){
-        return brandService.queryAll();
+    public List<TbBrand> findAll() {
+//        return brandService.queryAll();
+        return brandService.findAll();
+    }
+
+    @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                               @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+        return brandService.findPage(page, rows);
     }
 }
